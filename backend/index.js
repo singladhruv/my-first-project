5683eaceda967cbeb7135f2b1e7aa51be25f0195
Lucifer4602 <dhruvsingla183@gmail.com>
2024-06-26 22:08:31 +0530

    updated urls

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ connectToDB();
 
 // middlewares
 const corsConfig = {
-  origin: "http://localhost:3000", // specify your front-end URL
+  origin: "https://ecommerce-dhruv.netlify.app/", // specify your front-end URL
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
