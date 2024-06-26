d301474f4afde1e614b1e4d96da73413007f373f
Lucifer4602 <dhruvsingla183@gmail.com>
2024-06-26 22:25:49 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosi = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:8000",
+  baseURL: "https://ecommerce-api-nj1u.onrender.com",
 });
